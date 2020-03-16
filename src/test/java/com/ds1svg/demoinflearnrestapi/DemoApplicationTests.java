cb33abf80a751ee425bce426c26113f17dc1f2bd
sam <3161728@naver.com>
2020-03-16 13:54:53 +0900

    project creation

---
+++ src/test/java/com/ds1svg/demoinflearnrestapi/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ds1svg.demoinflearnrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
